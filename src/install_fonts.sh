712e8ed84b61b72f93fb475333399f2753c12fc4
Luke Rogers <lukeroge@gmail.com>
2024-11-05 21:57:20 +0000

    Realised previous approach will not work due to weirdness

--- src/install_fonts.sh
+++ src/install_fonts.sh
@@ -1,5 +1,30 @@
-#!/bin/bash
-git clone --depth 1 https://github.com/ryanoasis/nerd-fonts.git  
+declare -a fonts=(
+  0xProto
+  DroidSansMono
+  RobotoMono
+  Ubuntu
+  UbuntuMono
+)
 
-cd nerd-fonts/
-bash ./install.sh -S 0xProto
+version=$(curl -s 'https://api.github.com/repos/ryanoasis/nerd-fonts/releases/latest' | jq -r '.name')
+if [ -z "$version" ] || [ "$version" = "null" ]; then
+  version="v3.2.1"
+fi
+fonts_dir="/usr/share/fonts"
+
+if [[ ! -d "$fonts_dir" ]]; then
+  mkdir -p "$fonts_dir"
+fi
+
+for font in "${fonts[@]}"; do
+  zip_file="${font}.zip"
+  download_url="https://github.com/ryanoasis/nerd-fonts/releases/download/${version}/${zip_file}"
+  echo "Downloading $download_url"
+  wget "$download_url"
+  unzip -o "$zip_file" -d "$fonts_dir"  # Added the -o option here to allow replacing
+  rm "$zip_file"
+done
+
+find "$fonts_dir" -name 'Windows Compatible' -delete
+
+fc-cache -fv
